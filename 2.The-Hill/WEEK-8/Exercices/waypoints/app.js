e705cc2776406affd269eafbcfb2e8a772a6e005
Radad El Jaidi <radad1998@gmail.com>
2024-06-05 16:18:30 +0200

    waypoints

---
+++ 2.The-Hill/WEEK-8/Exercices/waypoints/app.js
@@ -0,0 +1,21 @@
+// Waypoint to detect scrolling down from Section 1 to Section 2
+new Waypoint({
+    element: document.getElementById('section2'),
+    handler: function(direction) {
+        if (direction === 'down') {
+            alert('Are you sure? Things are wild down there.');
+        }
+    },
+    offset: '0%' // Trigger when top of section 2 reaches the top of the viewport
+});
+
+// Waypoint to detect scrolling up from Section 2 to Section 1
+new Waypoint({
+    element: document.getElementById('section2'),
+    handler: function(direction) {
+        if (direction === 'up') {
+            alert('I told you!');
+        }
+    },
+    offset: '100%' // Trigger when bottom of section 2 reaches the bottom of the viewport
+});
